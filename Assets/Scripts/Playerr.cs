70ecac833fb33b43c79638d1a9bd39602c798ce2
penguiin1 <104720787+penguiin1@users.noreply.github.com>
2024-01-11 15:29:59 +0900

    test2

--- Assets/Scripts/Playerr.cs
+++ Assets/Scripts/Playerr.cs
@@ -1,27 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Playerr : MonoBehaviour
 {
-    public float speed;
+    public float speed; // 플레이어 이동속도
     public Vector2 inputVec;
 
-    Rigidbody2D rigid;
-
+    Rigidbody2D pRigid;
+    SpriteRenderer pSprite;
+    
+    // 컴포넌트 가져오기
     private void Awake()
     {
-        rigid = GetComponent<Rigidbody2D>();
+        pRigid = GetComponent<Rigidbody2D>();
+        pSprite = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
-    }
+    // 위치이동으로 구현
     private void FixedUpdate()
     {
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
-        rigid.MovePosition(rigid.position + nextVec);
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        pRigid.MovePosition(pRigid.position + nextVec);
+    }
+
+    private void LateUpdate()
+    {
+        if(inputVec.x != 0)
+        {
+            pSprite.flipX = inputVec.x > 0;
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
     }
 }
